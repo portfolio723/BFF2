2a61ce17ac98b2150cf974aad880eb2b2b730283
Sandeep <agencysandesigns@gmail.com>
2025-12-17 09:45:31 +0000

    Try fixing this error: `Runtime ReferenceError: Cannot access 'firebaseC

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,18 +1,8 @@
 
-"use client";
-
-import { initializeApp, getApp, getApps, type FirebaseOptions } from 'firebase/app';
+import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getAuth, connectAuthEmulator } from 'firebase/auth';
 import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
-
-export const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
+import { firebaseConfig } from './config';
 
 export function initializeFirebase() {
   if (getApps().length > 0) {
