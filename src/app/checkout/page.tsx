16d9e69ec44befe5edce400044ba26a99d1008da
Sandeep <agencysandesigns@gmail.com>
2025-12-19 09:26:58 +0000

    Error occured during checkout after the  the user fills the data in the

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -9,6 +9,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Badge } from "@/components/ui/badge";
 import {
   Select,
   SelectContent,
@@ -675,3 +676,5 @@ export default function CheckoutPage() {
     </section>
   );
 };
+
+    
