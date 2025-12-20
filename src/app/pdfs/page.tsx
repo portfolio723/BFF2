93ec8dca0b4cb8df4e8e2b1593747fc77a2ea3ef
Sandeep <agencysandesigns@gmail.com>
2025-12-20 04:36:58 +0000

    Also make the grid layout for the mobile responsive for the PDFs page in

--- src/app/pdfs/page.tsx
+++ src/app/pdfs/page.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 import { useState, useMemo } from "react";
 import { PdfCard } from "@/components/PdfCard";
@@ -95,7 +96,7 @@ export default function PdfsPage() {
 
       <main className="mb-20">
         {filteredPdfs.length > 0 ? (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-10">
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-10">
             {filteredPdfs.map((pdf) => (
               <PdfCard 
                 key={pdf.id} 
