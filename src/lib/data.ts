c3ba400a867135b1a626e7db77bfa563c11f2c68
Sandeep <agencysandesigns@gmail.com>
2025-12-24 06:03:03 +0000

    update the Engineering PDF's downloadable links:
    Advanced Engineering

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -233,14 +233,14 @@ export const economicsPdfs: Pdf[] = [
 ];
     
 export const engineeringPdfs: Pdf[] = [
-  { id: "eng1", title: "Advanced Engineering Mathematics", author: "Erwin Kreyszig", downloadUrl: "#", category: "Engineering", description: "", coverImage: {url: "", hint: ""} },
-  { id: "eng2", title: "Computer Networking", author: "N/A", downloadUrl: "#", category: "Engineering", description: "", coverImage: {url: "", hint: ""} },
-  { id: "eng3", title: "Control System Reference Book", author: "N/A", downloadUrl: "#", category: "Engineering", description: "", coverImage: {url: "", hint: ""} },
-  { id: "eng4", title: "Introduction to Algorithms", author: "Cormen", downloadUrl: "#", category: "Engineering", description: "", coverImage: {url: "", hint: ""} },
-  { id: "eng5", title: "Electrical Machines", author: "S. K. Sahdev", downloadUrl: "#", category: "Engineering", description: "", coverImage: {url: "", hint: ""} },
-  { id: "eng6", title: "Engineering Mechanics", author: "N/A", downloadUrl: "#", category: "Engineering", description: "", coverImage: {url: "", hint: ""} },
-  { id: "eng7", title: "Heat and Mass Transfer", author: "Rajput", downloadUrl: "#", category: "Engineering", description: "", coverImage: {url: "", hint: ""} },
-  { id: "eng8", title: "Higher Engineering Mathematics", author: "B.S. Grewal", downloadUrl: "#", category: "Engineering", description: "", coverImage: {url: "", hint: ""} },
+  { id: "eng1", title: "Advanced Engineering Mathematics", author: "Erwin Kreyszig", downloadUrl: "https://mega.nz/file/ioITUB5I#Xsid6rqNZVTtYXNGcr7qziFsR6lMKf0B12_83P6GwDg", category: "Engineering", description: "", coverImage: {url: "", hint: ""} },
+  { id: "eng2", title: "Computer Networking", author: "N/A", downloadUrl: "https://mega.nz/file/H0ZRmYoR#MBzbCEgNYEempVYs_GZRGfLKW5W0vzWQI4sU1PbwTgc", category: "Engineering", description: "", coverImage: {url: "", hint: ""} },
+  { id: "eng3", title: "Control System Reference Book", author: "N/A", downloadUrl: "https://mega.nz/file/H1AgADZJ#SFK5xaSJlRyP9QgAKsEVAjBMgsfkfE2CLToAi-TUDxk", category: "Engineering", description: "", coverImage: {url: "", hint: ""} },
+  { id: "eng4", title: "Introduction to Algorithms", author: "Cormen", downloadUrl: "https://mega.nz/file/T0wxBBrZ#rGNZszwIYpWvqJyOH-D9EdlMUV9-Fw-CVlXQMXBtm4w", category: "Engineering", description: "", coverImage: {url: "", hint: ""} },
+  { id: "eng5", title: "Electrical Machines", author: "S. K. Sahdev", downloadUrl: "https://mega.nz/file/PtRi2aJZ#rxConMc-nuRcn7ufXxMKa5dyNPxHAyUWjhJ5agmhVvg", category: "Engineering", description: "", coverImage: {url: "", hint: ""} },
+  { id: "eng6", title: "Engineering Mechanics", author: "N/A", downloadUrl: "https://mega.nz/file/b1Yy1TLI#2RFoHZwrN4XV9BwEG5KV6UDWm6e4Dk7QxxcVo57zKEs", category: "Engineering", description: "", coverImage: {url: "", hint: ""} },
+  { id: "eng7", title: "Heat and Mass Transfer", author: "Rajput", downloadUrl: "https://mega.nz/file/zhYnTD6Z#NQ9HZ-Dpmgo-uZzYVD2XHTcKREnjVpTVXz0zM6OQF9g", category: "Engineering", description: "", coverImage: {url: "", hint: ""} },
+  { id: "eng8", title: "Higher Engineering Mathematics", author: "B.S. Grewal", downloadUrl: "https://mega.nz/file/v44ETbiQ#fUYfe5YqMlkIU6qkDkyZVCNULoDdVdooCvDR5nXrKNo", category: "Engineering", description: "", coverImage: {url: "", hint: ""} },
   { id: "eng9", title: "Digital Design", author: "M. Morris Mano", downloadUrl: "#", category: "Engineering", description: "", coverImage: {url: "", hint: ""} },
   { id: "eng10", title: "Machine Design", author: "R.S. Kurmi", downloadUrl: "#", category: "Engineering", description: "", coverImage: {url: "", hint: ""} },
   { id: "eng11", title: "Microelectronic Circuits", author: "Sedra and Smith", downloadUrl: "#", category: "Engineering", description: "", coverImage: {url: "", hint: ""} },
@@ -249,4 +249,3 @@ export const engineeringPdfs: Pdf[] = [
   { id: "eng14", title: "Strength of Material", author: "R.K. Bansal", downloadUrl: "#", category: "Engineering", description: "", coverImage: {url: "", hint: ""} },
   { id: "eng15", title: "Transmission and Distribution Text Book", author: "N/A", downloadUrl: "#", category: "Engineering", description: "", coverImage: {url: "", hint: ""} },
 ];
-
