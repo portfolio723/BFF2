76f20e5f71b69edcd86b3d36726550570b8ea564
Sandeep <agencysandesigns@gmail.com>
2025-12-31 05:28:00 +0000

    xml page created

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://your-website-url.com'; // Replace with your actual domain
+  const baseUrl = 'https://www.booksforfoster.org/'; // Replace with your actual domain
 
   const staticPages = [
     '/',
